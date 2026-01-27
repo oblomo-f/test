e92367e226db6d53da2f8175d130b46fba864f25
oblomo-f <82931753+oblomo-f@users.noreply.github.com>
2026-01-27 11:34:59 +0300

    Update install_nfqws_keenetic.sh

--- install_nfqws_keenetic.sh
+++ install_nfqws_keenetic.sh
@@ -2,28 +2,28 @@
 
 set -e
 
-echo "==> Обновление списка пакетов и установка зависимостей"
+printf "\033[32;1m Обновление списка пакетов и установка зависимостей.\033[0m\n"
 opkg update
 opkg install ca-certificates wget-ssl
 opkg remove wget-nossl || true
 
-echo "==> Загрузка и установка публичного ключа репозитория"
+printf "\033[32;1m Загрузка и установка публичного ключа репозитория\033[0m\n"
 wget -O /tmp/nfqws-keenetic.pub \
   https://anonym-tsk.github.io/nfqws-keenetic/openwrt/nfqws-keenetic.pub
 opkg-key add /tmp/nfqws-keenetic.pub
 
-echo "==> Добавление репозитория nfqws-keenetic"
+printf "\033[32;1m Добавление репозитория nfqws-keenetic\033[0m\n"
 echo "src/gz nfqws-keenetic https://anonym-tsk.github.io/nfqws-keenetic/openwrt" \
   > /etc/opkg/nfqws-keenetic.conf
 
-printf "\033[32;1mУстановка пакета nfqws-keenetic\033[0m\n"
+printf "\033[32;1mУстановка пакета nfqws\033[0m\n"
 opkg update
 opkg install nfqws-keenetic
 
 printf "\033[32;1m Установка веб-интерфейса\033[0m\n"
 opkg install nfqws-keenetic-web
 
-printf "\033[32;1m Готово! nfqws-keenetic успешно установлен.\033[0m\n"
+printf "\033[32;1m Готово! nfqws успешно установлен.\033[0m\n"
 
 
 printf "\033[31;1mAfter 10 second AUTOREBOOT ROUTER...\033[0m\n"
